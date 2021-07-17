9d3c6aa237dc361f638de4173e1237d946d2d639
Rob <robvangastel@hotmail.com>
2021-07-17 13:58:02 +0200

    Improve ablation script and unused genotype parsing limit skip-connection

--- metanas/scripts/ablation/run_ablation.sh
+++ metanas/scripts/ablation/run_ablation.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 # parameters
-# TODO: Add eval_freq and n
-EPOCHS=15
-WARM_UP_EPOCHS=5
+EPOCHS=1000
+WARM_UP_EPOCHS=500
 SEEDS=(0 1 2)
+EVAL_FREQ=100
+N=1 # 3, 5
 DS=omniglot
 
 echo "Start run of ablation studies, variables epochs = ${EPOCHS}, warm up variables = ${WARM_UP_EPOCHS}, seeds = ${SEEDS[@]}, dataset = ${DS}"
 
 # MetaNAS baseline
-EPOCHS=$EPOCHS WARM_UP=$WARM_UP_EPOCHS SEEDS=${SEEDS[@]} DS=$DS ./scripts/ablation/run_metanas_baseline.sh
+EPOCHS=$EPOCHS WARM_UP=$WARM_UP_EPOCHS SEEDS=${SEEDS[@]} DS=$DS N=$N EVAL_FREQ=$EVAL_FREQ ./scripts/ablation/run_metanas_baseline.sh
 
 # SharpDARTS
-EPOCHS=$EPOCHS WARM_UP=$WARM_UP_EPOCHS SEEDS=${SEEDS[@]} DS=$DS ./scripts/ablation/run_cosine_power_annealing.sh
-EPOCHS=$EPOCHS WARM_UP=$WARM_UP_EPOCHS SEEDS=${SEEDS[@]} DS=$DS ./scripts/ablation/run_alpha_regularization.sh
-EPOCHS=$EPOCHS WARM_UP=$WARM_UP_EPOCHS SEEDS=${SEEDS[@]} DS=$DS ./scripts/ablation/run_ss_sharpdarts.sh
+EPOCHS=$EPOCHS WARM_UP=$WARM_UP_EPOCHS SEEDS=${SEEDS[@]} DS=$DS N=$N EVAL_FREQ=$EVAL_FREQ ./scripts/ablation/run_cosine_power_annealing.sh
+EPOCHS=$EPOCHS WARM_UP=$WARM_UP_EPOCHS SEEDS=${SEEDS[@]} DS=$DS N=$N EVAL_FREQ=$EVAL_FREQ ./scripts/ablation/run_alpha_regularization.sh
+EPOCHS=$EPOCHS WARM_UP=$WARM_UP_EPOCHS SEEDS=${SEEDS[@]} DS=$DS N=$N EVAL_FREQ=$EVAL_FREQ ./scripts/ablation/run_ss_sharpdarts.sh
 
 # P-DARTS
-EPOCHS=$EPOCHS WARM_UP=$WARM_UP_EPOCHS SEEDS=${SEEDS[@]} DS=$DS ./scripts/ablation/run_ss_approximation.sh
-EPOCHS=$EPOCHS WARM_UP=$WARM_UP_EPOCHS SEEDS=${SEEDS[@]} DS=$DS ./scripts/ablation/run_ss_regularization.sh
+EPOCHS=$EPOCHS WARM_UP=$WARM_UP_EPOCHS SEEDS=${SEEDS[@]} DS=$DS N=$N EVAL_FREQ=$EVAL_FREQ ./scripts/ablation/run_ss_approximation.sh
+EPOCHS=$EPOCHS WARM_UP=$WARM_UP_EPOCHS SEEDS=${SEEDS[@]} DS=$DS N=$N EVAL_FREQ=$EVAL_FREQ ./scripts/ablation/run_ss_regularization.sh
