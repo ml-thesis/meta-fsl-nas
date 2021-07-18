35ca24824cbdaddf5262ac348ea30d42f2037ca3
Rob <robvangastel@hotmail.com>
2021-07-18 17:39:40 +0200

    Adjust spacing ablation script

--- metanas/scripts/ablation/run_metanas_baseline.sh
+++ metanas/scripts/ablation/run_metanas_baseline.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 DATASET=$DS
-# DATASET_DIR=/home/rob/Git/meta-fsl-nas/data
 DATASET_DIR=/home/TUE/20184291/meta-fsl-nas/data
 
 for SEED in ${SEEDS}
 do
     TRAIN_DIR=/home/TUE/20184291/meta-fsl-nas/metanas/results/ablation/${DS}_train_metanas/${DS}_train_metanas_$SEED
-	mkdir -p $TRAIN_DIR
+    mkdir -p $TRAIN_DIR
 
     args=(
         # Execution
