fc6882bc2b6e1149a8a1f4aea227b66c147404a7
Rob <robvangastel@hotmail.com>
2021-07-24 09:19:49 +0200

    Add baseline experiment script

---
+++ metanas/scripts/baseline/run_baseline.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source /home/TUE/20184291/miniconda3/etc/profile.d/conda.sh
+source activate metanas
+
+# parameters
+EPOCHS=500
+WARM_UP_EPOCHS=250
+SEEDS=(1)
+EVAL_FREQ=100
+N=3
+DS=omniglot # triplemnist
+
+echo "Start run of baseline studies, variables epochs = ${EPOCHS}, warm up variables = ${WARM_UP_EPOCHS}, seeds = ${SEEDS[@]}, dataset = ${DS}"
+
+# MetaNAS baseline
+EPOCHS=$EPOCHS WARM_UP=$WARM_UP_EPOCHS SEEDS=${SEEDS[@]} DS=$DS N=$N EVAL_FREQ=$EVAL_FREQ ./scripts/ablation/run_metanas.sh
+
+# Best ablation components
+# EPOCHS=$EPOCHS WARM_UP=$WARM_UP_EPOCHS SEEDS=${SEEDS[@]} DS=$DS N=$N EVAL_FREQ=$EVAL_FREQ ./scripts/ablation/run_metanas.sh
