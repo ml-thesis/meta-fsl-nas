7210a5d8291a3b8a83adf20dd2e6f41c96824cdb
Rob <robvangastel@hotmail.com>
2021-09-03 22:59:33 +0200

    Update DQN and DRQN with improved performance on POMDP CartPole

---
+++ metanas/metanas/meta_optimizer/agents/tests/DQN/test_drqn.py
@@ -0,0 +1,30 @@
+import argparse
+
+from metanas.meta_optimizer.agents.utils.run_utils import setup_logger_kwargs
+from metanas.meta_optimizer.agents.DQN.drqn import DRQN
+from metanas.meta_optimizer.agents.utils.env_wrappers import CartPolePOMDPWrapper
+
+import gym
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int, default=42, help="random seed")
+    parser.add_argument("--use_pomdp", action="store_true",
+                        help="Use POMDP CartPole environment")
+    args = parser.parse_args()
+
+    path = "CartPole/DRQN"
+    if args.use_pomdp:
+        path = "CartPole/DRQN_POMDP"
+        env = CartPolePOMDPWrapper(gym.make("CartPole-v1"))
+        test_env = CartPolePOMDPWrapper(gym.make("CartPole-v1"))
+    else:
+        env = gym.make("CartPole-v1")
+        test_env = gym.make("CartPole-v1")
+
+    logger_kwargs = setup_logger_kwargs(path, seed=args.seed)
+
+    qnet_kwargs = dict(hidden_size=64)
+    agent = DRQN(env, test_env, seed=args.seed, qnet_kwargs=qnet_kwargs,
+                 logger_kwargs=logger_kwargs)
+    agent.train_agent()
--- metanas/metanas/meta_optimizer/agents/tests/SAC/test_rl2_sac.py
+++ metanas/metanas/meta_optimizer/agents/tests/SAC/test_rl2_sac.py
@@ -1,8 +1,7 @@
 import argparse
 
 from metanas.meta_optimizer.agents.utils.run_utils import setup_logger_kwargs
-from metanas.meta_optimizer.agents.DQN.dqn import DQN
-# from metanas.meta_optimizer.agents.SAC.rl2_sac import RL2_SAC
+from metanas.meta_optimizer.agents.SAC.rl2_sac import SAC
 
 import gym
 from gym.wrappers import AtariPreprocessing
@@ -14,10 +13,10 @@ if __name__ == "__main__":
     env = gym.make("CartPole-v1")
     test_env = gym.make("CartPole-v1")
 
-    logger_kwargs = setup_logger_kwargs("DQN", seed=42)
+    logger_kwargs = setup_logger_kwargs("RL2_SAC", seed=42)
 
-    qnet_kwargs = dict(hidden_size=256)
+    ac_kwargs = dict(hidden_size=[256]*2)
     # Ignore the meta-model and config input
-    agent = DQN(env, test_env,  qnet_kwargs=qnet_kwargs,
+    agent = SAC(env, test_env, ac_kwargs=ac_kwargs,
                 logger_kwargs=logger_kwargs)
     agent.train_agent()
